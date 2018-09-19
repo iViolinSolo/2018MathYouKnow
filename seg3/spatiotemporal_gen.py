b67817e6a0fcc99c829442d9102e152e811ef275
ViolinSolo <i.violinsolo@gmail.com>
2018-09-19 08:47:33 +0800

    add pd in trr mapping

--- seg3/spatiotemporal_gen.py
+++ seg3/spatiotemporal_gen.py
@@ -4,6 +4,7 @@
 # Created on 19/09/2018
 from data.data_reader import DataReader
 import pandas as pd
+from pyecharts import Map
 import numpy as np
 
 df = pd.read_csv('./../data/kpro final/kpro-adv_ver-submission02.csv', index_col=0)
@@ -15,11 +16,24 @@ m = x.count()['eventid']
 df_plain = DataReader(name='./../data/gtd').df
 
 df_plain['CLUSTER_SCORE'] = df['CLUSTER_SCORE']
-df_tar = df_plain[['eventid', 'iyear', 'imonth', 'iday', 'region', 'country_txt', 'provstate', 'latitude', 'longitude', 'CLUSTER_SCORE']]
-df_tar = df_tar[(df_tar['iyear']==2015)|(df_tar['iyear']==2016)|(df_tar['iyear']==2017)]
+df_tar = df_plain[['eventid', 'iyear', 'imonth', 'iday', 'region', 'country_txt', 'provstate', 'latitude', 'longitude',
+                   'CLUSTER_SCORE']]
+df_tar = df_tar[(df_tar['iyear'] == 2015) | (df_tar['iyear'] == 2016) | (df_tar['iyear'] == 2017)]
 df_tar = df_tar[~(df_tar['longitude'].isna() | df_tar['latitude'].isna())]
 
-m = df_tar.groupby(by=['iyear', 'imonth', 'country_txt']).count()
+m = df_tar.groupby(by=['iyear', 'imonth', 'country_txt']).count()['eventid']
+n = m.reset_index()
+
+
+def plot_my_map(year: int, month: int):
+    df_tmp = n[(n['iyear'] == year) & (n['imonth'] == month)]
+    value = df_tmp['eventid'].values
+    attr = df_tmp['country_txt'].values
+
+    map = Map("%d年%2d月恐怖袭击频次分布图" % (year, month), width=1200, height=600)
+    map.add("", attr, value, maptype="world", is_visualmap=True, visual_text_color='#000')
+
+    map.render(path='./../data/seg3/%d_%2d_terr_freq_overall.html' % (year, month))
 
 # _geo = df_tar[['eventid', 'latitude', 'longitude']].values
 # _data = df_tar[['eventid', 'CLUSTER_SCORE']].values
